b9419fa76505c0c3fb35f71876693ae3d0f24bc5
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-12 15:56:59 +0600

    part 7 has done

--- src/module4/static.ts
+++ src/module4/static.ts
@@ -13,3 +13,15 @@ class Counter {
 // const instance2 = new Counter();
 console.log(Counter.increment()); // 0-1
 console.log(Counter.increment()); // 1-2
+
+
+class MyClass {
+  static myStaticProp = 'Hello, world!';
+
+  static myStaticMethod() {
+    console.log('This is a static method.');
+  }
+}
+
+console.log(MyClass.myStaticProp); // Output: 'Hello, world!'
+MyClass.myStaticMethod(); // Output: 'This is a static method.'
