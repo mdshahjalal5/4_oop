1e743a084b9a3835e332810aa55e54530b24cdef
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-13 15:53:25 +0600

    part 7 has done

--- src/module4/abstraction_pr.ts
+++ src/module4/abstraction_pr.ts
@@ -1,15 +1,18 @@
 interface  Ieducatino_platform {
     name:string, 
     version:string
-    estalished:number, 
-    (x:string):void
+    established?:number, 
+    teach(x:string):void
 }
 
-class Education_platform {
+class Education_platform implements Ieducatino_platform {
     version: string;
     constructor(public name:string, version_any:string, public established:number){
         this.name = name, 
         this.version = version_any, 
         this.established = established
+    }
+    teach(x:string):void{
+        console.log(x)
     }
 }
