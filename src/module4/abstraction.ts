7e3bddd66c19851d8562e843a2b880ca6c3a58ee
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-27 11:44:38 +0600

    done

--- src/module4/abstraction.ts
+++ src/module4/abstraction.ts
@@ -1,66 +1,66 @@
-//   //interface
+  //interface
 
-// // interface IVehicle {
-// //   name: string;
-// //   model: string;
-// // }
-
-// // const vehicle: IVehicle = {
-// //   name: "Car",
-// //   model: "2000",
-// // };
-
-// interface Vehicle {
-//   startEngine(): void;
-//   stopEngine(): void;
-//   move?(): void;
+// interface IVehicle {
+//   name: string;
+//   model: string;
 // }
 
-// class Car implements Vehicle {
+// const vehicle: IVehicle = {
+//   name: "Car",
+//   model: "2000",
+// };
+
+interface Vehicle {
+  startEngine(): void;
+  stopEngine(): void;
+  move?(): void;
+}
+
+class Car implements Vehicle {
+  constructor(
+    public name: string,
+    public brand: string,
+    public model: number
+  ) {}
+  startEngine(): void {
+    console.log(" I am starting engine... ");
+  }
+  stopEngine(): void {
+    console.log(" I am syopping engine");
+  }
+  // move(): void {
+  //   console.log(" I am syopping engine");
+  // }
+}
+
+const vehicle1 = new Vehicle("Car", "Toyota", 2000);
+
+// //abstract class
+
+// abstract class Vehicle {
 //   constructor(
 //     public name: string,
 //     public brand: string,
 //     public model: number
 //   ) {}
-//   startEngine(): void {
+//   abstract startEngine(): void 
+//   abstract stopEngine(): void 
+//   move(): void {
+//     console.log(" I am syopping engine");
+//   }
+ 
+// }
+
+// class Car extends Vehicle{
+//   startEngine(): void   {
 //     console.log(" I am starting engine... ");
 //   }
 //   stopEngine(): void {
-//     console.log(" I am syopping engine");
+//     console.log(" I am stopping engine... ");
 //   }
-//   // move(): void {
-//   //   console.log(" I am syopping engine");
-//   // }
+  
 // }
 
-// const vehicle1 = new Vehicle("Car", "Toyota", 2000);
-
-// // //abstract class
-
-// // abstract class Vehicle {
-// //   constructor(
-// //     public name: string,
-// //     public brand: string,
-// //     public model: number
-// //   ) {}
-// //   abstract startEngine(): void 
-// //   abstract stopEngine(): void 
-// //   move(): void {
-// //     console.log(" I am syopping engine");
-// //   }
- 
-// // }
-
-// // class Car extends Vehicle{
-// //   startEngine(): void   {
-// //     console.log(" I am starting engine... ");
-// //   }
-// //   stopEngine(): void {
-// //     console.log(" I am stopping engine... ");
-// //   }
-  
-// // }
-
-// const car1= new Vehicle('Car','Honda',2015)
-// car1.
+const car1= new Vehicle('Car','Honda',2015)
+car1.
 
